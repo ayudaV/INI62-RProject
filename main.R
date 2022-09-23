e1c4bdfd3cf9afbfd4dbb1041ee490dbb6e7ab3d
VShoji <69880007+VShoji@users.noreply.github.com>
2022-09-23 09:17:49 -0300

    Update main.R

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
 names <- c("horario", "temp", "vento", "umid", "sensa")
-cepagri <- read.csv("C:/Users/terad/OneDrive/Documentos/GitHub/INI62-RProject/cepagri.csv", header = FALSE, sep = ";", col.names = names)
+cepagri <- read.csv("./cepagri.csv", header = FALSE, sep = ";", col.names = names)
 
 cepagri$temp <- as.numeric(cepagri$temp)
 cepagri <- na.omit(cepagri) 
@@ -9,5 +9,7 @@ cepagri <- na.omit(cepagri)
 cepagri$horario <- as.POSIXct(cepagri$horario,format="%d/%m/%Y-%H:%M",tz=Sys.timezone())
 
 cepagri <- subset(cepagri, sensa <= 60)
+
+#cepagri = subset(cepagri, umid >= 10)
 
 summary(cepagri)
