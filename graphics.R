9ed145904a4460700b895d60b147f51397619eac
Terada Yuhji <cc20122@g.unicamp.br>
2022-10-02 23:47:02 -0300

    Update graphics.R

--- graphics.R
+++ graphics.R
@@ -15,16 +15,24 @@ c2016 <- subset(cepagri, horario >= "2016/01/01")
 c2016 <- subset(c2016, horario < "2017/01/01")
 
 c2016$horario <- as.POSIXct(c2016$horario)
-ggplot(c2016, aes(x = temp, y = umid)) +
-  geom_point(shape=1, fill="red", color="darkred", size=1) +
-
-
+  
 ggplot(c2016, aes(temp, sensa)) +
   geom_point(fill="red", color="darkred") +
   geom_smooth(method = "lm", formula = y ~ x, se=FALSE) +
-  labs(title = "Temperatura x Sensação Termica",
+  labs(title = "Temp x Sensação Termica",
        x = "Temperatura ºC", y = "Sensação Termica ºC")
 
+ggplot(c2016, aes(vento, sensa)) +
+  geom_point(fill="green", color="darkgreen") +
+  geom_smooth(method = "lm", formula = y ~ x, se=FALSE) +
+  labs(title = "Vento x Sensação Termica",
+       x = "Vento km/h", y = "Sensação Termica ºC")
+
+ggplot(c2016, aes(umid, sensa)) +
+  geom_point(fill="blue", color="darkblue") +
+  geom_smooth(method = "lm", formula = y ~ x, se=FALSE) +
+  labs(title = "Umidade x Sensação Termica",
+       x = "Umidade %", y = "Sensação Termica ºC")
 
 
 
